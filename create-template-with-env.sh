59ea2edcce3fdfe040574f0384952f50ca43914d
Tamir <tamir@inject.top>
2019-12-04 15:34:51 +0800

    EXPOSE port

--- create-template-with-env.sh
+++ create-template-with-env.sh
@@ -1,33 +1,40 @@
 #!/bin/sh
 set -e
+if [[ -n "$EXPOSE_PORT" ]]; then
+export EXPOSE_PORT_STR="$(cat << EOF
+    ports:
+      $EXPOSE_PORT:5000
+EOF)"
+fi
 ssh $SSH_REMOTE "mkdir -p $WORKDIR"
 if [[ -n "$STATCK_CUSTOMIZE_FILE" ]]; then
 cat $STATCK_CUSTOMIZE_FILE | ssh $SSH_REMOTE "cat > $STATCK_FILE"
 else
-ssh $SSH_REMOTE "cat > $STATCK_FILE" << EOF
-    version: '3.7'
-    services:
-      ${SERVICE_NAME}:
-        image: "\${IMAGE}"
-        env_file:
-          - "\${ENV_FILE}"
-        healthcheck:
-          test: ["CMD", "curl", "-f", "http://127.0.0.1:5000/healthcheck"]
-          interval: 5s
-          timeout: 10s
-          retries: 3
-          start_period: 15s
-        deploy:
-          update_config:
-            parallelism: 1
-            delay: 1s
-            failure_action: rollback
-          replicas: \${REPLICAS}
-          restart_policy:
-            condition: on-failure
-            delay: 5s
-            max_attempts: 5
-            window: 120s
+ssh $SSH_REMOTE "cat | sed -e '/^[[:space:]]*$/d' > $STATCK_FILE" << EOF
+version: '3.7'
+services:
+  ${SERVICE_NAME}:
+    image: "\${IMAGE}"
+    env_file:
+      - "\${ENV_FILE}"
+$EXPOSE_PORT_STR
+    healthcheck:
+      test: ["CMD", "curl", "-f", "http://127.0.0.1:5000/healthcheck"]
+      interval: 5s
+      timeout: 10s
+      retries: 3
+      start_period: 15s
+    deploy:
+      update_config:
+        parallelism: 1
+        delay: 1s
+        failure_action: rollback
+      replicas: \${REPLICAS}
+      restart_policy:
+        condition: on-failure
+        delay: 5s
+        max_attempts: 5
+        window: 120s
 EOF
 fi
 set | grep ^SWARM_ | sed -e "s/^SWARM_//" -e 's/='\''/=/' -e 's/'\''$//' | ssh $SSH_REMOTE "cat > $ENV_FILE"
--- upgrade-service.sh
+++ upgrade-service.sh
@@ -2,5 +2,5 @@
 set -e
 ssh $SSH_REMOTE 'bash -s' <<EOF
   chmod 600 $ENV_FILE
-  IMAGE=$IMAGE ENV_FILE=$ENV_FILE REPLICAS=$REPLICAS SERVICE_NAME=$SERVICE_NAME docker stack deploy --with-registry-auth --compose-file=$STATCK_FILE ${NAMESPACE}
+  IMAGE=$IMAGE ENV_FILE=$ENV_FILE REPLICAS=$REPLICAS SERVICE_NAME=$SERVICE_NAME EXPOSE_PORT:$EXPOSE_PORT docker stack deploy --with-registry-auth --compose-file=$STATCK_FILE ${NAMESPACE}
 EOF
